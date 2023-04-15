eb9e5f0ff45d52a62abc098171b18beddaf38e7c
LAPTOP-KJ40RCEH\omega <omegaxzj@gmail.com>
2023-04-15 19:59:31 -0400

    Animator basic

--- Assets/Scripts/Animator.cs
+++ Assets/Scripts/Animator.cs
@@ -6,12 +6,14 @@ public class Animator : MonoBehaviour
 {
     private Animator _animator;
     private SpriteRenderer _sprite;
+    private Movement _movement;
     
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
         _sprite = GetComponent<SpriteRenderer>();
+        _movement = GetComponent<Movement>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,25 @@ public class Animator : MonoBehaviour
         //todo set bool Dash
         //todo set bool canMove
     }
+
+    public void SetHorizontalMovement(float x,float y, float yVel)
+    {
+        //todo SetFloat x
+        //todo SetFloat y
+        //todo SetFloat yVel
+    }
     
+    public void Flip(int side)
+    {
+        //上墙状态,如果对齐则不执行
+        if (_movement.ifOnWall())
+        {
+            if ((side == -1 && _sprite.flipX)||(side == 1 && !_sprite.flipX))
+                return;
+        }
+        
+        bool state = (side != 1);
+        _sprite.flipX = state;
+    }
     
 }
