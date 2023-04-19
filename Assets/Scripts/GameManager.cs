a37ee006f5aaf35b467bf70c0b2240569c9139f0
Xiao <sz3456@nyu.edu>
2023-04-19 12:54:31 -0400

    debug test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))//later change to when the player reaches the goal point
+        {
+            SceneManager.LoadScene("Level 1");
+            //debug test
+        }
+    }
+}
